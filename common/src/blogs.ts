bbfaefcfa585f97a7a21c18e7f66e02b8b9419cd
Jatan Dudeja <dudeja.jatan04@gmail.com>
2025-02-27 23:06:12 +0530

    fix: fixed create blogs dto.

--- common/src/blogs.ts
+++ common/src/blogs.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createPostZod = z.object({
   description: z.string().min(10).nonempty(),
-  title: z.string().min(2).max(20).nonempty(),
+  title: z.string().min(2).nonempty(),
 });
 
 export type CreatePostDTO = z.infer<typeof createPostZod>;
